5e845c3ca06eaf252b60fad63123b02b5b94f010
Ahmed Habib <66479808+ahmedhabiib@users.noreply.github.com>
2021-08-20 21:05:38 +0300

    added styles and ejs updated

--- app.js
+++ app.js
@@ -15,38 +15,18 @@ const connection = mysql.createConnection({
 	database: 'joinus'
 });
 
-const person = {
-	email: faker.internet.email(),
-	created_at: faker.date.past()
-};
-
 connection.connect(function(err) {
 	if (err) throw err.message;
 	console.log('Connected!');
 });
 
-const allUsers = 'SELECT * FROM users';
-const firstUser =
-	'SELECT monthname(created_at) as month, count(*) as total FROM users group by month order by total desc';
-const createUser = 'INSERT INTO users SET ? ';
-const yahoUsers = 'SELECT  count(*) as Provider from users  group by email like "%@yahoo.com" ';
-
 app.get('/', (req, res) => {
-	connection.query(yahoUsers, function(error, result, fields) {
+	const q = 'SELECT COUNT(*) as total FROM users';
+	connection.query(q, function(error, results, fields) {
 		if (error) throw error.sqlMessage;
-		console.log(result.total);
-		res.render('home', { result });
+		const total = results[0].total;
+		res.render('home', { total });
 	});
-});
-
-app.get('/create', (req, res) => {
-	connection.query(createUser, person, (err, res, fields) => {
-		if (err) {
-			throw err.sqlMessage;
-		}
-	});
-
-	res.redirect('/');
 });
 
 app.listen(5000, () => {
